da25624a2a29e3652e9450597b5c5313e9548c01
monica <monicaalina58@gmail.com>
2020-12-28 17:04:31 -0500

    Web-scraping

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,20 +1,50 @@
+# Import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 import pandas as pd
 
+# Create an instance of Flask
 app = Flask(__name__)
 
-app.config["MONGO_URL"] = 'mongodb://localhost:27017/Mars_db'
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
 
-@app.route("/")
-def index():
-    MarsData = mongo.db.collection.find_one()
-    return render_template("index.html", list = MarsData)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/MarsDB")
+#app.config['MONGO_URL'] = 'mongodb://localhost:27017/MarsDB'
+#mongo = PyMongo(app)
 
 
-@app.route("/scrape")
-def Scrape():
-    Scrape.scrape_all()
+
+# Route to render index.html template using data from Mongo
+@app.route('/')
+def Home():
+    
+    # Find one record of data from the mongo database
+    MarsScraping = mongo.db.MarsDB.find_one()
+
+    # Return template and data
+    return render_template('index.html', MarsData = MarsScraping)
+
+# Route that will trigger the scrape function
+@app.route('/scrape')
+def scrape():
+    
+    # Run the scrape function
+    
+    MarsData = scrape_mars.MarsNews()
+    MarsData = scrape_mars.FeaturedImage()
+    MarsData = scrape_mars.MarsFacts()
+    MarsData = scrape_mars.Hemispheres()
+
+    # Update the Mongo database using update and upsert = True
+    
+    mongo.db.MarsDB.update({}, MarsData, upsert = True)
+
+    # Redirect back to home page
+
+    #print('Redirect')
     return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,186 +1,79 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+# Import dependencies
 
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import requests
-
-
-# In[2]:
+import json 
+import splinter 
 
 def Initialization():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     return Browser('chrome', **executable_path, headless=False)
 
+MarsScraping = {}
 
-# ### NASA Mars News
-
-# In[3]:
-
-MarsNews = {}
-
-def News():
+def MarsNews():
     browser = Initialization()
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
-
-
-# In[4]:
-
-
     html = browser.html
-
-
-# In[5]:
-
-
     soup = bs(html,"html.parser")
-
-
-# In[6]:
-
-
-    print(soup.prettify())
-
-
-# In[7]:
-
 
     Content = soup.find('div', class_ = 'content_page')
 
-
-# In[8]:
-
-
-    NewsTitle = Content.find_all('div', class_ = 'content_title')
-    print(NewsTitle[0].text.strip())
-
-
-# In[9]:
-
+    Title = Content.find_all('div', class_ = 'content_title')
+    NewsTitle = Title[0].text.strip()
+    MarsScraping['NewsTitle'] = NewsTitle
 
     NewsParagraph = soup.find('div', class_ = 'article_teaser_body').text
-    print (NewsParagraph)
+    MarsScraping['NewsParagraph'] = NewsParagraph
 
-    return MarsNews
-
-
-# ### JPL Mars Space Images - Featured Image
-
-# In[10]:
+    return MarsScraping
 
 def FeaturedImage():
     browser = Initialization()
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'  
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
-
-
-# In[11]:
-
-
     html = browser.html
-
-
-# In[12]:
-
-
     soup = bs(html,"html.parser")
 
-
-# In[13]:
-
-
     browser.find_by_id("full_image").click()
-
-
-# In[15]:
-
-
     browser.find_by_text("more info     ").click()
-
-
-# In[16]:
-
-
     html = browser.html
     soup = bs(html,"html.parser")
     featured_image = soup.find("img", class_ = "main_image")["src"]
 
-
-# In[17]:
-
-
-    featured_image
-
-
-# In[18]:
-
-
     featured_image_url = f"https://www.jpl.nasa.gov{featured_image}"
     featured_image_url
 
-    return MarsNews
+    MarsScraping['featured_image_url'] = featured_image_url
+    
+    return MarsScraping
 
-
-# ### Mars Facts
-
-# In[19]:
-
-def Facts():
+def MarsFacts():
+    browser = Initialization()
     FactsUrl = 'https://space-facts.com/mars/'
-
-
-# In[20]:
-
-
+    browser.visit(FactsUrl)
     Facts = pd.read_html(FactsUrl)
-    Facts
+    #Facts
+    
+    FactsTable = Facts[0].rename(columns = {0:"Profile",1:"Data"})
+    FactsTable = FactsTable.set_index("Profile")
+    MarsFactsData = FactsTable.to_html()
 
+    MarsScraping['MarsFactsData'] = MarsFactsData
 
-# In[21]:
-
-
-    Facts[0].rename(columns = {0:"Profile",1:"Data"})
-
-
-# In[22]:
-
-
-    Facts[1]
-
-    return MarsNews
-
-
-# ### Mars Hemispheres
-
-# In[23]:
+    return MarsScraping
 
 def Hemispheres():
     browser = Initialization()
     HomeURL = 'https://astrogeology.usgs.gov'
     AstrogelogySite = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(AstrogelogySite)
-
-
-# In[24]:
-
-
     html = browser.html
-
-
-# In[25]:
-
-
     soup = bs(html,"html.parser")
-
-
-# In[26]:
-
 
     HemispheresEnhanced = soup.find('div', class_ = 'collapsible results')
     MarsHemispheresEnhanced = HemispheresEnhanced.find_all('div', class_='item')
@@ -206,24 +99,33 @@ def Hemispheres():
             Img_url = Img_link.find('li').a['href']
 
     
-            Img_dict = {}
-            Img_dict['Title'] = Title
-            Img_dict['Img_url'] = Img_url
+            #Img_dict = {}
+            #Img_dict['Title'] = Title
+            #Img_dict['Img_url'] = Img_url
+
+            HemData = dict({"Title": Title, "Img_url": Img_url})
+
+
         
-            Hemisphere_Image_URLs.append(Img_dict)
+            Hemisphere_Image_URLs.append(HemData)
      
-            print(Hemisphere_Image_URLs)
-            print("--------------------------------------")
+            #print(Hemisphere_Image_URLs)
+            #print("--------------------------------------")
         
         except:
             pass
 
+    MarsScraping['Hemisphere_Image_URLs'] = Hemisphere_Image_URLs
 
-# In[27]:
+    return MarsScraping
 
 
-    Hemisphere_Image_URLs
 
-    return MarsNews
+
+
+
+
+
+
 
 
